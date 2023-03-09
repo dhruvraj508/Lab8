74f21e6b04bcba54150dc3f7ec12c865a419e96f
Dhruvraj Singh <dhruvrajsingh@Dhruvrajs-MacBook-Pro.local>
2023-03-08 17:57:03 -0700

    our tests should fail (deleteCity)

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -34,5 +34,13 @@ public class CustomListTest {
         list.addCity(new City("Estevan", "SK"));
         assertEquals(list.getCount(),listSize + 1);
     }
+    
+    @Test
+    public void deleteCityTest(){
+        list = MockCityList();
+        int listSize = list.getCount();
+        list.deleteCity(new City("Estevan", "SK"));
+        assertEquals(list.getCount(), listSize - 1);
+    }
 
 }
